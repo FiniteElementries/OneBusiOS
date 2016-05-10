bb1ad1dce5a34e6e4de459ca5c62758c090a31a8
FiniteElement <harryzheng1991@gmail.com>
2016-05-10 09:18:12 +0800

    init

---
+++ Alamofire/DerivedData/Alamofire/Build/Intermediates/Alamofire.build/Debug-iphonesimulator/Alamofire iOS.build/Objects-normal/x86_64/Alamofire_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/harryzheng/Dropbox/My\ Documents/Co-curriculumn/Github/iOS/api_framework/Alamofire/DerivedData/Alamofire/Build/Intermediates/Alamofire.build/Debug-iphonesimulator/Alamofire\ iOS.build/DerivedSources/Alamofire_vers.c
